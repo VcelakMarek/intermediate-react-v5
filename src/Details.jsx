fa9ac5491b888f0800b96c9f7ee0222d854f04e8
Marek Vcelak <vcelak.marek@gmail.com>
2023-01-25 22:26:44 +0100

    Update Details.jsx

--- src/Details.jsx
+++ src/Details.jsx
@@ -8,64 +8,61 @@ import Carousel from "./Carousel";
 import Modal from "./Modal";
 
 const Details = () => {
-    const [showModal, setShowModal] = useState(false);
-    const navigate = useNavigate();
-    
-    // eslint-disable-next-line no-unused-vars
-    const [_, setAdoptedPet] = useContext(AdoptedPetContext)
-    const { id } = useParams();
-    const results = useQuery(["details", id], fetchPet);     //useQuery(["cache key",queryKey], function)
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
 
-    if (results.isLoading) {
-        return (
-            <div className="loading-pane">
-                <h2 className="loader">🌀</h2>
-            </div>
-        );
-    }
+  // eslint-disable-next-line no-unused-vars
+  const [_, setAdoptedPet] = useContext(AdoptedPetContext);
+  const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet); //useQuery(["cache key",queryKey], function)
 
-    const pet = results.data.pets[0];
-
+  if (results.isLoading) {
     return (
-        <div className="details">
-            <Carousel images={pet.images} />
-            <div> 
-                <h1>{pet.name}</h1>
-                <h2>
-                    {pet.animal} — {pet.breed} — {pet.city}, {pet.state}
-                    <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
-                    <p>{pet.description}</p>
-
-                    {
-                        showModal ? 
-                        (
-                            <Modal>
-                                <div>
-                                    <h1>Would you like to adopt? {pet.name}?</h1>
-                                    <div className="buttons">
-                                        <button onClick={() => {
-                                            setAdoptedPet(pet)
-                                            navigate("/");
-                                        }}
-                                        >
-                                            Yes
-                                        </button>
-                                        <button onClick={() => setShowModal(false)}>No</button>
-                                    </div>
-                                </div>
-                            </Modal>
-                        ) : null
-                    }
-                </h2>
-            </div>
-        </div>
-    )
-  };
-
-  export default function DetailsErrorBoundary(props) {
-    return (
-        <ErrorBoundary>
-            <Details {...props} />
-        </ErrorBoundary>
-    )
+      <div className="loading-pane">
+        <h2 className="place-content-center">🌀</h2>
+      </div>
+    );
   }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <Carousel images={pet.images} />
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} — {pet.breed} — {pet.city}, {pet.state}
+          <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
+          <p>{pet.description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt? {pet.name}?</h1>
+                <div className="buttons">
+                  <button
+                    onClick={() => {
+                      setAdoptedPet(pet);
+                      navigate("/");
+                    }}
+                  >
+                    Yes
+                  </button>
+                  <button onClick={() => setShowModal(false)}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
