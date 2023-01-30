d3eea09acf1f617942a82241b1ee0805ed4a2fd2
Marek Vcelak <vcelak.marek@gmail.com>
2023-01-30 11:18:59 +0100

    init branch typescript

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from "react";
-import { Link, Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Suspense } from "react";
-import AdoptedPetContext from "./AdoptedPetContext";
-import Details from "./Details";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
-      cacheTime: Infinity,
-      suspense: true,
-    },
-  },
-});
+      cacheTime: Infinity
+    }
+  }  
+})
 
 const App = () => {
   const adoptedPet = useState(null);
   return (
-    <QueryClientProvider client={queryClient}>
-      <AdoptedPetContext.Provider value={adoptedPet}>
-        <Suspense
-          fallback={
-            <div className="loading-pane">
-              <h2 className="loader">🌀</h2>
-            </div>
-          }
-        >
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AdoptedPetContext.Provider value={adoptedPet}>
           <header>
             <Link to="/"> Adopt Me!</Link>
           </header>
@@ -35,11 +28,12 @@ const App = () => {
             <Route path="/details/:id" element={<Details />} />
             <Route path="/" element={<SearchParams />} />
           </Routes>
-        </Suspense>
-        ;
-      </AdoptedPetContext.Provider>
-    </QueryClientProvider>
+        </AdoptedPetContext.Provider>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 };
 
-export default App;
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
